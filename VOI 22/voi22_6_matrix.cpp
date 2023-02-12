c4659d814588ff1a18acf3bd4658566fd90684ae
nmhienbn <65478890+nmhienbn@users.noreply.github.com>
2023-02-12 17:11:07 +0700

    Update voi22_6_matrix.cpp

--- VOI 22/voi22_6_matrix.cpp
+++ VOI 22/voi22_6_matrix.cpp
@@ -1,36 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e7 + 5, mod = 1e9 + 7;
+const int maxn = 1e6 + 5;
+const int mod = 1e9 + 7;
+const int SQRT = 31623; // sqrt(10^9)
 
 int m, n;
-long long res;
-int cnt[35], mu[35];
-int p[maxn], dem;
+int p[SQRT + 5], dem;
+vector<int> Primes;
+int c[maxn];
+int CntC[35], CntD[35];
 
 void add(long long &x, long long y)
 {
     x = (x + y) % mod;
 }
-
 void Eratosthenes()
 {
-    for (int i = 2; i <= 4000; i++)
-        if (!p[i])
-        {
-            p[i] = ++dem;
-            for (int j = i * i; j < maxn; j += i)
-                if (!p[j])
-                {
-                    p[j] = dem;
-                }
-        }
-
-    for (int i = 4001; i <= 10000000; i++)
-        if (!p[i])
-        {
-            p[i] = ++dem;
+    Primes.push_back(1);
+    for (int i = 2; i < SQRT; i++) if (!p[i]) {
+            Primes.push_back(i);
+            for (int j = i * i; j < SQRT; j += i)
+                p[j] = 1;
         }
 }
+
+int calc(int x, int i, int sign = 1)
+{
+    if (i == 0)
+        return sign * x;
+    if (i == 1)
+        return sign * (x - x / 2);
+    if (i == 2)
+        return sign * (x - x / 2 - x / 3 + x / 6);
+
+    long long result = 0;
+    while (i > 3){
+        if (i < Primes.size() && x < Primes[i + 1])
+            return result + sign;
+        i--;
+        result += calc(x / Primes[i + 1], i, -sign);
+    }
+    return result + sign * (x - x / 2 - x / 3 + x / 6 - x / 5 + x / 10 + x / 15 - x / 30);
+}
+
+int count_bit_i_th(int n, int i){ // count the number of positive integer not exceeding n that its i-th bit = 1
+    int n0 = n | ((1 << i) - 1);
+    return ((n0 + 1) >> 1) - min((1 << (i - 1)), n0 - n);
+}
+
+void solve(int n, int CntC[])
+{
+    int CntN = n - 1;
+    int idN = 0;
+    c[0] = 1;
+    for (int i = 1, _ = Primes.size(); i < _; i++)
+    {
+        int prime = Primes[i];
+        if (1LL * prime * prime > n)
+            break;
+        c[i] = calc(n / prime, i - 1);
+        idN = i;
+        CntN -= c[i];
+    }
+    for (int i = 0; i <= idN; i++)
+        for (int j = 0; j <= 30; j++)
+            if ((i >> j) & 1)
+                CntC[j] += c[i];
+    for (int j = 0; j <= 30; j++)
+        CntC[j] += count_bit_i_th(idN + CntN, j + 1) - count_bit_i_th(idN, j + 1);
+}
+
+long long res;
+
 
 int main()
 {
@@ -40,25 +81,12 @@ int main()
     freopen("matrix.out", "w", stdout);
     Eratosthenes();
     cin >> m >> n;
-
-    for (int i = 1; i <= n; i++)
-        for (int j = 0; j <= 30; j++)
-            cnt[j] += (p[i] >> j) & 1;
-
-    for (int i = 0; i <= 30; i++)
-        mu[i] = 1 << i;
-
-    for (int i = 1; i <= m; i++)
-        for (int j = 0; j <= 30; j++)
-            if ((p[i] >> j) & 1)
-            {
-                add(res, (long long)(n - cnt[j]) * mu[j]);
-            }
-            else
-            {
-                add(res, (long long)cnt[j] * mu[j]);
-            }
-
+    solve(n, CntC);
+    solve(m, CntD);
+    for (int i = 0; i <= 30; i++){
+        add(res, (1LL << i) * CntC[i] % mod * (m - CntD[i]));
+        add(res, (1LL << i) * (n - CntC[i]) % mod * CntD[i]);
+    }
     add(res, (long long)m * n);
     cout << res;
     return 0;
